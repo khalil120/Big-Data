5f3ed343c42a2124a3f93d3289d529a6006be388
khalil120 <khaleel.kblawe@gmail.com>
2020-07-27 16:25:12 +0300

    Update final.R
    Co-Authored-By: Khalil Qablawi  <khaleel.kblawe@gmail.com>

--- final.R
+++ final.R
@@ -6,3 +6,9 @@ data = fread("GlobalLandTemperaturesByCity.csv")
 
 #deleting NA rows
 data <- data[!is.na(AverageTemperature)]
+
+#changing the class of dt field from char to DATE
+data[, dt := as.Date(data$dt)]
+data[, Year := substr(dt,1,4)]
+#MPY = measure per year
+data[, MPY := .N, by = Year]
