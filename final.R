c6f4c47de38519126161e047de256419a78e0e0b
khalil120 <khaleel.kblawe@gmail.com>
2020-07-28 17:44:57 +0300

    Update final.R
    filter the file to contain data netwen 01/1800 till 08/2013
    
    Co-Authored-By: Khalil Qablawi  <khaleel.kblawe@gmail.com>

--- final.R
+++ final.R
@@ -9,4 +9,7 @@ data <- data[!is.na(AverageTemperature)]
 
 #changing the class of dt field from char to DATE
 data[, dt := as.Date(data$dt)]
-data[, Year := substr(dt,1,4)]
+data[, Year := as.integer(substr(dt,1,4))]
+data <- data[Year > 1799]
+#MPC = measure per city -> this field will be deleted
+data[, MPC := .N, by = City]
