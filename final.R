24864148a88931ce57cf7a0421183ce893a74122
khalil120 <khaleel.kblawe@gmail.com>
2020-08-11 14:32:35 +0300

    final.R updated
    Co-Authored-By: Khalil Qablawi  <khaleel.kblawe@gmail.com>

--- final.R
+++ final.R
@@ -1,15 +1,18 @@
 library(data.table)
 
+#my goal is to divide the data-table to 4-sub Tables(WEST,EAST,NORTH,SOUTH)
 
 #load the data to data table
 data = fread("GlobalLandTemperaturesByCity.csv")
 
 #deleting NA rows
 data <- data[!is.na(AverageTemperature)]
-
+data[,AverageTemperatureUncertainty := NULL]
 #changing the class of dt field from char to DATE
 data[, dt := as.Date(data$dt)]
 data[, Year := as.integer(substr(dt,1,4))]
-data <- data[Year > 1799]
-#MPC = measure per city -> this field will be deleted
-data[, MPC := .N, by = City]
+#range of years: 1800 to 2012
+data <- data[Year > 1799 & Year < 2013]
+
+data[, RegionLati := substr(Latitude,nchar(Latitude),nchar(Latitude))]
+data[, RegionLong := substr(Longitude,nchar(Longitude),nchar(Longitude))]
